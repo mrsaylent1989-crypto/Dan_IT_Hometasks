1bff16b4743e80673125a587fa02cedd85e8e272
Vitaliy_Fedorov <Mr.saylent1989@gmail.com>
2025-10-11 14:11:41 +0300

    home work 1

---
+++ HOMETASKS/src/main/java/java_hw_1/program.java
@@ -0,0 +1,19 @@
+package java_hw_1;
+
+public class program {
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 9;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
