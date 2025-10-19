0c8326c339ec9adff4aaf8f6ba091165e295e1b2
Vitaliy_Fedorov <Mr.saylent1989@gmail.com>
2025-10-19 11:49:50 +0300

    first java code

---
+++ HOMETASKS/src/main/java/Hw_1/Task_1.java
@@ -0,0 +1,18 @@
+package Hw_1;
+
+public class Task_1 {
+    public static void main(String[] args){
+        int a = 7;
+        int b = 9;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
